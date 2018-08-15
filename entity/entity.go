988a37dc34c78f77f778a149664bd3f1c3247cac
Gang Yang <gang.yang.o@nio.com>
2018-08-15 20:07:13 +0800

    add task entity

--- entity/entity.go
+++ entity/entity.go
@@ -4,11 +4,13 @@ import (
 	"github.com/satori/go.uuid"
 )
 
+// 实体对象
 type Entity struct {
 	// 实体 Id
 	Id string `json:"id"`
 }
 
+// 生成新的 uuid
 func (e *Entity) NewId() {
 	e.Id = uuid.NewV4().String()
 }
--- entity/task/task.go
+++ entity/task/task.go
@@ -1,6 +1,7 @@
 package task
 
 import (
+	"time"
 	"github.com/mickeygo/timergo/entity"
 )
 
@@ -16,4 +17,33 @@ type Task struct {
 
 	// 是否禁用
 	IsDisabled 	bool	`json:"disabled"`
+
+	CreatedDate time.Time `json:"created_date"`
+
+	UpdatedDate time.Time `json:"updated_date"`
+}
+
+func NewTask(name string, remote string) Task {
+	return Task {
+		Name: name
+		Remote: remote
+		IsDisabled: false
+		CreatedDate: time.Now()
+	}
+}
+
+// 启用任务
+func (t *Task) Enable() {
+	if (t.IsDisabled) {
+		t.IsDisabled = false;
+		t.UpdatedDate = time.Now()
+	}
+}
+
+// 禁用任务
+func (t *Task) Disable() {
+	if (!t.IsDisabled) {
+		t.IsDisabled = true;
+		t.UpdatedDate = time.Now()
+	}
 }
