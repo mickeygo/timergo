e2de8faddea4e77be4bfbe61397c943fcc7c01ec
yangganggood <yanggangc@163.com>
2018-08-28 20:21:33 +0800

    opentracing

---
+++ tracing/tracing.go
@@ -0,0 +1,7 @@
+package tracing
+
+import (
+	opentracing "github.com/opentracing/opentracing-go"
+	opentracing_ext "github.com/opentracing/opentracing-go/ext"
+	opentracing_log "github.com/opentracing/opentracing-go/log"
+)
